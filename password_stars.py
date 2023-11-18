d8119f4b45f3f4aeff6f2dc699f1037cd7e64b4c
Vincent Thendean <vincent.thendean@my.jcu.edu.au>
2023-11-18 14:26:13 +0800

    Add password check programs

--- password_stars.py
+++ password_stars.py
@@ -2,7 +2,7 @@ print("Password must be 10 characters or longer")
 passwordHidden = input("Input Password: ")
 
 while len(passwordHidden)<10:
-    print("Password must be 10 characters or longer")
+    print("Password must be 10 characters or longe r")
     passwordHidden = input("Input Password: ")
 
 print('*'*len(passwordHidden) )
